14f1315696c5c44a0492667eb33be1faea7ab696
Zhong Yong Qiang <258003787@qq.com>
2016-08-15 10:38:28 +0800

    2016-08-15 10:37

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
+import { default as Video, Controls, Play, Mute, Seek, Fullscreen, Time, Overlay } from 'react-html5video'
 import styles from './index.less'
 
 let self
@@ -10,15 +11,52 @@ class Index extends Component {
     super(props)
 
     self = this
+
+    this.state = {
+      isPlay: false
+    }
+
+    this.props.socket.on('open', function (data) {
+      if(data.user == 'small') {
+        self.setState({
+          isPlay: data.data == 5
+        })
+
+        if(data.data == 5) {
+          self.refs.video.load()
+          self.refs.video.play()
+        } else {
+
+        }
+      }
+    })
   }
 
   componentDidMount() {
 
   }
 
+  componentWillUnmount() {
+    self.props.socket.removeAllListeners(['open'])
+  }
+
   render() {
     return (
     	<div className={styles.bg}>
+        {
+          this.state.isPlay ?
+          <Video loop muted
+            ref="video"
+            style={{width: '100%'}}
+            poster="http://sourceposter.jpg"
+            onCanPlayThrough={() => {
+
+            }}>
+            <source src="video.mp4" type="video/mp4"/>
+          </Video>
+          :
+          ''
+        }
       </div>
     )
   }
