f8eb9fc6c8ff565401f546fbfdca870a5ade9478
NowUI <nowui@163.com>
2016-10-11 13:53:49 +0800

    2016-10-11 13:53

--- src/components/3/1/Index.jsx
+++ src/components/3/1/Index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
+import { Spin } from 'antd'
+import Helper from '../../../common/Helper'
 import styles from './Index.less'
 
 let self
@@ -10,16 +12,98 @@ class Detail extends Component {
     super(props)
 
     self = this
+
+    this.state = {
+      isLoad: false,
+      id: '',
+      object: {
+        id: '',
+        pagecontent: ''
+      },
+      page: 1,
+      limit: 1,
+      total: 0
+    }
+
+    this.props.socket.on('open', function (data) {
+      if(data.user == 'small') {
+        self.setState({
+          id: data.data,
+          page: 1
+        })
+
+        self.load()
+      }
+    })
+
+    this.props.socket.on('up', function (data) {
+      if(data.user == 'small') {
+        if(self.state.page > 1) {
+          self.setState({
+            page: self.state.page - 1
+          })
+
+          self.load()
+        }
+      }
+    })
+
+    this.props.socket.on('down', function (data) {
+      if(data.user == 'small') {
+        console.log(self.state.page)
+        console.log(self.state.total)
+        if(self.state.page < self.state.total) {
+          self.setState({
+            page: self.state.page + 1
+          })
+
+          self.load()
+        }
+      }
+    })
   }
 
   componentDidMount() {
 
   }
 
+  componentWillUnmount() {
+    self.props.socket.removeAllListeners(['open'])
+    self.props.socket.removeAllListeners(['up'])
+    self.props.socket.removeAllListeners(['down'])
+  }
+
+  load = function() {
+    self.setState({
+      isLoad: true
+    })
+
+    Helper.ajax({
+      url: '/services/lhsgkContent/' + self.state.id + '-' + self.state.page + '-' + self.state.limit,
+      data: {
+
+      },
+      success: function(data) {
+        self.setState({
+          object: data.list[0],
+          total: data.totalPage
+        })
+      },
+      complete: function() {
+        self.setState({
+          isLoad: false
+        })
+      }
+    })
+  }
+
   render() {
     return (
-      <div className={styles.bg}>
-      </div>
+      <Spin size="large" spinning={this.state.isLoad}>
+        <div className={styles.bg}>
+          <div className={styles.content} dangerouslySetInnerHTML={{__html: this.state.object.content}}></div>
+        </div>
+      </Spin>
     )
   }
 }
